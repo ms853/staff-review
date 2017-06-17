cbbeec8b9be2228527ba10c968ec59c2478ef222
Michael Sakyiama <ms853@student.le.ac.uk>
2017-06-17 07:24:04 +0100

    add the models to the web app

---
+++ Sytner-College/Sytner-College/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SytnerCollege.Models
+{
+    public class Student
+    {
+        public int ID { get; set; } //Student ID which uniquely identifies each student. 
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public DateTime DOB { get; set; } //Date of Birth 
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
