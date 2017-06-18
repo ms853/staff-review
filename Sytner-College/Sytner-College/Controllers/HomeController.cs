34df4f6f5a7f20e2c2c7db68b30bbd5b283e1356
Michael Sakyiama <ms853@student.le.ac.uk>
2017-06-18 11:06:34 +0100

    add the page feature to the student index page, to limit the number of student records shown on each page. Also modify the about page.

--- Sytner-College/Sytner-College/Controllers/HomeController.cs
+++ Sytner-College/Sytner-College/Controllers/HomeController.cs
@@ -3,21 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SytnerCollege.Data;
+using SytnerCollege.Models.CollegeViewModels;
 
 namespace Sytner_College.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly CollegeContext _context;
+
+        public HomeController(CollegeContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
+        public async Task<ActionResult> About()
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+            return View(await data.AsNoTracking().ToListAsync());
         }
 
         public IActionResult Contact()
--- Sytner-College/Sytner-College/Controllers/StudentsController.cs
+++ Sytner-College/Sytner-College/Controllers/StudentsController.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System;
 using System.Threading.Tasks;
+using SytnerCollege;
 using SytnerCollege.Data;
 using SytnerCollege.Models;
 
@@ -18,10 +19,21 @@ namespace Sytner_College.Controllers
         }
 
         // GET: Students
-        public async Task<IActionResult> Index(string sortOrder, string searchString)
+        public async Task<IActionResult> Index(string sortOrder, string searchString, string currentFilter, int? page)
         {
+            ViewData["CurrentSort"] = sortOrder;
             ViewData["NameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
             ViewData["DateSortParm"] = sortOrder == "Date" ? "date_desc" : "Date";
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
             ViewData["CurrentSearch"] = searchString;
 
             var students = from s in _context.Students
@@ -48,7 +60,8 @@ namespace Sytner_College.Controllers
                     students = students.OrderBy(s => s.LastName);
                     break;
             }
-            return View(await students.AsNoTracking().ToListAsync());
+            int pageSize = 3;
+            return View(await PaginatedList<Student>.CreateAsync(students.AsNoTracking(), page ?? 1, pageSize));
         }
 
         // GET: Students/Details/5
