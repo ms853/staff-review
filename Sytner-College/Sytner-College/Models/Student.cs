6b18c94105ac705a99051f5c6fdb40a69302311a
Michael Sakyiama <ms853@student.le.ac.uk>
2017-06-18 17:32:34 +0100

    Add migration to the database and add data format to the enrollment date and D.O.B

--- Sytner-College/Sytner-College/Models/Student.cs
+++ Sytner-College/Sytner-College/Models/Student.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.ComponentModel.DataAnnotations;
 namespace SytnerCollege.Models
 {
     public class Student
     {
-        public int ID { get; set; } //Student ID which uniquely identifies each student. 
+        //PrimaryKey for unique identify for every student. 
+        public int ID { get; set; }  
         public string LastName { get; set; }
         public string FirstMidName { get; set; }
+        
+        //Added Data Annotation to explicitly specify the date format for both enrollent and dob.
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
+
+        [DataType(DataType.Date)] 
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime DOB { get; set; } //Date of Birth 
 
         public ICollection<Enrollment> Enrollments { get; set; }
